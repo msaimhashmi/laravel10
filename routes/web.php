3090dd93d5be688306685d625599837fcae36673
Saim Hashmi <saimhashmii433@gmail.com>
2023-02-22 14:09:45 +0500

    Generate Secure Passwords

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/secure-password', function(){
+    // Generate Secure Passwords
+    // You'll find a new password() method on the Str class. This method - which can be adjusted to your preferred criteria - will generate a secure password for you.
+    // Examples
+    echo 'random passowrd = '.str()->password();
+    echo '<br>';
+    echo 'limit passowrd = '.str()->password(10);
+    echo '<br>';
+    echo 'without integere passowrd = '.str()->password(10, numbers: false);
+    echo '<br>';
+    echo 'without symbols passowrd = '.str()->password(10, symbols: false);
+});
