7e7e2a852de3a5168753ce074c16cbd069ce54f4
Saim Hashmi <saimhashmii433@gmail.com>
2023-02-22 13:54:52 +0500

    Native PHP Types

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,13 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+
+    // here is the example below
+        /**
+         * Define the application's command schedule.
+         */
+        protected function schedule(Schedule $schedule): void
+        {
+            // $schedule->command('inspire')->hourly();
+        }
+    // example end
+
 
     /**
      * Register the commands for the application.
