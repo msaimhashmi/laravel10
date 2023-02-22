4a27272773552d8ac03b66fe37123092c1f8ff03
Saim Hashmi <saimhashmii433@gmail.com>
2023-02-22 14:56:42 +0500

    new process facade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Process\Pool;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +30,37 @@ Route::get('/secure-password', function(){
     echo 'without integere passowrd = '.str()->password(10, numbers: false);
     echo '<br>';
     echo 'without symbols passowrd = '.str()->password(10, symbols: false);
+});
+
+
+// NEW PROCESS FACADE
+
+/* This facade, built on top of Symfony's excellent Process component, allows you to easily invoke and execute shell commands, 
+ and even run them asynchronously in the background. */
+
+Route::get('/process-facade', function(){
+    $result = Process::run('ls -la')->output();
+    return $result->output();
+});
+
+Route::get('/process-facade-fake', function(){
+    // all process would be fake by using this
+    Process::fake();
+
+    // we can fake only scpecific process by using this 
+    // Process::fake(['git log' => 'a git log is fake']);
+    
+    Process::run('git log');
+    Process::run('ls -la');
+});
+
+
+Route::get('/process-facade-pool', function(){
+    [$first, $second, $third] = Process::concurrently(function (Pool $pool) {
+        $pool->command('cat first.txt');
+        $pool->command('cat second.txt');
+        $pool->command('cat third.txt');
+    });
+     
+    return $first->output();
 });
